892f28cc5fabab87b5e7360a6a208bc07a561913
Adeel <Adeel@LAPTOP-V844ODPF>
2023-04-10 18:21:01 -0400

    HW11 Part A Done

---
+++ src/main/java/com/mycompany/alrajaab11/AmericanPowerPlug.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.alrajaab11;
+
+/**
+ *
+ * @author Adeel
+ */
+public class AmericanPowerPlug implements AmericanPlug {
+    @Override
+    public void provideElectricity() {
+        System.out.println("American power plug is providing electricity.");
+    }
+}
