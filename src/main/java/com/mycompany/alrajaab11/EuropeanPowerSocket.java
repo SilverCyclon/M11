83f294ad1b4fb3fac0459de37ec9da4b11d3cfaf
Adeel <Adeel@LAPTOP-V844ODPF>
2023-04-10 18:21:34 -0400

    HW11 Part A Done

---
+++ src/main/java/com/mycompany/alrajaab11/EuropeanPowerSocket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.alrajaab11;
+
+/**
+ *
+ * @author Adeel
+ */
+public class EuropeanPowerSocket implements EuropeanSocket {
+    @Override
+    public void getElectricity() {
+        System.out.println("European power socket is getting electricity.");
+    }
+}
